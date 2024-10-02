cfe7060913f3a2b9c5e8d5ac12d0f8bcb4e9b744
Dinesh10731j <dinesh1122@gmail.com>
2024-10-02 13:06:13 +0545

    Suspense  added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Banner from '@/components/Banner';
 import BookList from '@/components/BookList';
+import { Suspense } from 'react';
 
 const Page = () => {
   return (
     <>
       <section className='container mx-auto flex flex-col items-center overflow-hidden  p-6'>
         <Banner />
+
+        <Suspense fallback='Loading...'>
         <BookList />
+        </Suspense>
+        
       </section>
     </>
   );
