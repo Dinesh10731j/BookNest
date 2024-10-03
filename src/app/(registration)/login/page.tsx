470191791929112213c94038f16fa436d79c393b
Dinesh10731j <dinesh1122@gmail.com>
2024-10-03 16:29:08 +0545

    login validation completed

--- src/app/(registration)/login/page.tsx
+++ src/app/(registration)/login/page.tsx
@@ -17,6 +17,8 @@ const Login = () => {
   const onLogin:SubmitHandler<loginType> = (data)=>{
     console.log(data);
 
+    reset();
+
   }
   return (
     <div className="flex gap-7 flex-col min-h-screen bg-gray-100">
@@ -39,26 +41,28 @@ const Login = () => {
                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-700">Email</label>
                 <input 
                   type="email" 
-                  id="email" 
-                  required 
+                 
                   className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-300" 
                   placeholder="Enter your email" 
                   {...register('email',{required:'E-mail is required'})}
                 />
-                {errors.email && <p className='text-red-700 text-sm'>{errors.email?.message}</p>}
+                
+                {errors.email && <p className='text-red-700 text-sm '>{errors.email?.message}</p>}
               </div>
 
               <div className="mb-6">
                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-700">Password</label>
                 <input 
                   type="password" 
-                  id="password" 
-                  required 
+               
+              
                   className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-300" 
                   placeholder="Enter your password" 
                   {...register('password',{required:'Password is required'})}
                 />
+                 {errors.password && <p className='text-red-700 text-sm'>{errors.email?.message}</p>}
               </div>
+            
 
               <button 
                 type="submit" 
