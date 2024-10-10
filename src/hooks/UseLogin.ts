e3e3eb3c578445fbad6b86a3a4adc5b758daa423
Dinesh10731j <dinesh1122@gmail.com>
2024-10-10 18:09:12 +0545

    Login endpoint intregation completed

--- src/hooks/UseLogin.ts
+++ src/hooks/UseLogin.ts
@@ -1,33 +1,34 @@
 import { Endpoints } from "../../config/endpoints";
-const { Login:loginEndpoint} = Endpoints; 
+const { Login: loginEndpoint } = Endpoints;
 import axiosInstance from "../../config/axiosInstance";
 import { loginType } from "../../types/Type";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
 const Login = async (logindata: loginType): Promise<loginType> => {
-    try {
-        const response = await axiosInstance.post(loginEndpoint, logindata);
-        return response.data; 
-
-    } catch (error: unknown) {
+  try {
+    const response = await axiosInstance.post(loginEndpoint, logindata);
+    return response.data;
+  } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
-      
-        const errorMessage = error.response?.data?.message || "An unknown error occurred.";
-        throw new Error(errorMessage);
+      const errorMessage =
+        error.response?.data?.message || "An unknown error occurred.";
+      throw new Error(errorMessage);
     } else {
-       
-        throw new Error("An unexpected error occurred.");
+      throw new Error("An unexpected error occurred.");
     }
-}
+  }
 };
 
- export const UseUserLogin = ()=>{
-    return useMutation({
-        mutationKey:['userLogin'],mutationFn:Login,onSuccess:()=>{
-           console.log('Login Successfull')
-        },onError:(err)=>{
-            console.log(err.message);
-        }
-    })
- }
+export const UseUserLogin = () => {
+  return useMutation({
+    mutationKey: ["userLogin"],
+    mutationFn: Login,
+    onSuccess: () => {
+      console.log("Login Successfull");
+    },
+    onError: (err) => {
+      console.log(err.message);
+    },
+  });
+};
